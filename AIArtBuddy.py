5100e7637f9f2aeb466d22744d8bb45b58b8aa0e
Nasif Tajwar <nasiftazwar@gmail.com>
2025-04-26 12:26:42 +0600

    Update AIArtBuddy.py

--- AIArtBuddy.py
+++ AIArtBuddy.py
@@ -7,30 +7,25 @@ api_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImQ5Mzc0YzUwMW
 user_input = input("Enter a description for the image: ")
 url = "https://api.monsterapi.ai/v1/generate/txt2img"
 headers = {"Authorization": f"Bearer {api_token}"}
-response = requests.post(url,
-                         json={
-                             "prompt": user_input,
-                             "safe_filter": True
-                         },
-                         headers=headers)
+response = requests.post(url, json={"prompt": user_input, "safe_filter": True}, headers=headers)
 
 if response.status_code == 200:
-    print(
-        "Loading ... The image may take a few seconds.") 
+    print("Loading... The image may take a few seconds.")
     process_id = response.json().get("process_id")
 
     while True:
-        status_data = requests.get(
-            f"https://api.monsterapi.ai/v1/status/{process_id}",
-            headers=headers).json()  
+        status_data = requests.get(f"https://api.monsterapi.ai/v1/status/{process_id}", headers=headers).json()
         status = status_data.get("status")
-        print(status_data)
+        # print(status_data)
 
         if status == "COMPLETED":
-            image_url = status_data['result']['output'][
-                0]  
-            img = Image.open(BytesIO(requests.get(image_url).content)).show()
+            image_url = status_data['result']['output'][0]
+            img = Image.open(BytesIO(requests.get(image_url).content))
+            img.show()
+            print()
             break
         elif status == "FAILED":
             print("Image generation failed.")
             break
+else:
+    print(f"Error: {response.status_code}")
